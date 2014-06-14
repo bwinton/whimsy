57ffdc568cb2377333d68ea339d4334adbc6095b
Mike Conley <mike.d.conley@gmail.com>
2014-06-14 01:02:58 -0400

    Add invincibility powers when paint flashing is enabled.

--- data/audio-content.js
+++ data/audio-content.js
@@ -1,6 +1,25 @@
+let kUrlAudioMap = {};
+
 self.port.on('play-once', function(aPayload) {
   let url = aPayload.url;
   new Audio(url).play();
+});
+
+self.port.on('play-loop', function(aPayload) {
+  let url = aPayload.url;
+  let audio = new Audio(url);
+  audio.loop = true;
+  audio.play();
+  kUrlAudioMap[url] = audio;
+});
+
+self.port.on('play-loop-stop', function(aPayload) {
+  let url = aPayload.url;
+  let audio = kUrlAudioMap[url];
+  if (audio) {
+    audio.pause();
+    delete kUrlAudioMap[url];
+  }
 });
 
 self.port.emit('loaded');
