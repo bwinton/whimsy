77c55d0968ac281365be16b90fb5c5e938cafb44
Blake Winton <bwinton@latte.ca>
2014-08-02 12:28:35 -0400

    Get icons working in nightly again.

--- data/newtabicons-content.js
+++ data/newtabicons-content.js
@@ -28,48 +28,53 @@ function updateThumbnails() {
   }
 
   var toggle = document.getElementById("newtab-toggle");
-  switch (self.options.showPref) {
-  case 0:
-    toggle.setAttribute("title", "Show whimsical thumbnails on hover");
-    break;
-  case 1:
-    toggle.setAttribute("title", "Always show whimsical thumbnails");
-    break;
-  case 2:
-    toggle.setAttribute("title", "Hide the new tab page");
-    break;
-  case 3:
-    toggle.setAttribute("title", "Show the plain new tab page");
+  if (toggle) {
+    switch (self.options.showPref) {
+    case 0:
+      toggle.setAttribute("title", "Show whimsical thumbnails on hover");
+      break;
+    case 1:
+      toggle.setAttribute("title", "Always show whimsical thumbnails");
+      break;
+    case 2:
+      toggle.setAttribute("title", "Hide the new tab page");
+      break;
+    case 3:
+      toggle.setAttribute("title", "Show the plain new tab page");
+    }
   }
 
   for (let i = 0; i < thumbnails.length; ++i) {
     let thumb = thumbnails[i];
     let thumbs = thumb.getElementsByClassName('newtab-thumbnail');
-    if (thumbs.length) {
-      thumb = thumbs[0];
+    if (!thumbs.length) {
+      thumbs = [thumb];
     }
 
-    switch (self.options.showPref) {
-    case 0:
-      thumb.style.backgroundImage = thumb.dataset.oldPreview;
-      thumb.removeEventListener("mouseover", mouseOverListener);
-      thumb.removeEventListener("mouseout", mouseOutListener);
-      break;
-    case 1:
-      thumb.style.backgroundImage = thumb.dataset.oldPreview;
-      thumb.addEventListener("mouseover", mouseOverListener);
-      thumb.addEventListener("mouseout", mouseOutListener);
-      break;
-    case 2:
-      thumb.style.backgroundImage = thumb.dataset.newPreview;
-      thumb.removeEventListener("mouseover", mouseOverListener);
-      thumb.removeEventListener("mouseout", mouseOutListener);
-      break;
-    case 3:
-      thumb.style.backgroundImage = thumb.dataset.oldPreview;
-      thumb.removeEventListener("mouseover", mouseOverListener);
-      thumb.removeEventListener("mouseout", mouseOutListener);
-    }
+    for (let i = 0; i < thumbs.length; i++) {
+      let thumb = thumbs[i];
+      switch (self.options.showPref) {
+      case 0:
+        thumb.style.backgroundImage = thumb.dataset.oldPreview;
+        thumb.removeEventListener("mouseover", mouseOverListener);
+        thumb.removeEventListener("mouseout", mouseOutListener);
+        break;
+      case 1:
+        thumb.style.backgroundImage = thumb.dataset.oldPreview;
+        thumb.addEventListener("mouseover", mouseOverListener);
+        thumb.addEventListener("mouseout", mouseOutListener);
+        break;
+      case 2:
+        thumb.style.backgroundImage = thumb.dataset.newPreview;
+        thumb.removeEventListener("mouseover", mouseOverListener);
+        thumb.removeEventListener("mouseout", mouseOutListener);
+        break;
+      case 3:
+        thumb.style.backgroundImage = thumb.dataset.oldPreview;
+        thumb.removeEventListener("mouseover", mouseOverListener);
+        thumb.removeEventListener("mouseout", mouseOutListener);
+      }
+    };
   }
 
   window.addEventListener("mousemove", function (e) {
@@ -77,13 +82,16 @@ function updateThumbnails() {
     for (let i = 0; i < thumbnails.length; ++i) {
       let thumb = thumbnails[i];
       let thumbs = thumb.getElementsByClassName('newtab-thumbnail');
-      if (thumbs.length) {
-        thumb = thumbs[0];
+      if (!thumbs.length) {
+        thumbs = [thumb];
       }
 
       let top = Math.round(e.clientY * 100 / window.innerHeight);
       let left = Math.round(e.clientX * 100 / window.innerWidth);
-      thumb.style.backgroundPosition = "top " + top + "% left " + left + "%";
+      for (let i = 0; i < thumbs.length; i++) {
+        let thumb = thumbs[i];
+        thumb.style.backgroundPosition = "top " + top + "% left " + left + "%";
+      }
     }
   });
 }
@@ -99,26 +107,30 @@ function addThumbnails(thumbnails) {
   for (let i = 0; i < thumbnails.length; ++i) {
     let thumb = thumbnails[i];
     let thumbs = thumb.getElementsByClassName('newtab-thumbnail');
-    if (thumbs.length) {
-      thumb = thumbs[0];
-    } else {
+    if (!thumbs.length) {
+      thumbs = [thumb];
+    }
+    for (let j = 0; j < thumbs.length; j++) {
+      let thumb = thumbs[j];
       thumb.style.backgroundSize = "cover";
       thumb.style.backgroundRepeat = "no-repeat";
       thumb.style.backgroundClip = "paddingBox";
+      thumb.dataset.newPreview = 'url("' + self.options.thumbs[i] + '")';
+      thumb.dataset.oldPreview = thumb.style.backgroundImage;
+      thumb.dataset.thumburl = self.options.thumbs[i];
     }
-    thumb.dataset.newPreview = 'url("' + self.options.thumbs[i] + '")';
-    thumb.dataset.oldPreview = thumb.style.backgroundImage;
-    thumb.dataset.thumburl = self.options.thumbs[i];
   }
   updateThumbnails();
 }
 
 function overrideToggle() {
-  // Tell the add-on when the toggle is clicked…
   var toggle = document.getElementById("newtab-toggle");
-  toggle.onclick = function () {
-    self.port.emit('toggle clicked', {});
-  };
+  if (toggle) {
+    // Tell the add-on when the toggle is clicked…
+    toggle.onclick = function () {
+      self.port.emit('toggle clicked', {});
+    };
+  }
   // And eventually it'll tell us what the new value of the pref is…
   self.port.on('showPrefUpdated', function (e) {
     self.options.showPref = e;
