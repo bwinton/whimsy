09426e6bd4d258443787d9edf81e5889717dd424
Blake Winton <bwinton@latte.ca>
2014-12-08 12:48:11 -0500

    Make sure we’re getting all the catgifs gifs, too.

--- checklinks.py
+++ checklinks.py
@@ -8,22 +8,45 @@ WARNING = '\033[93m'
 FAIL = '\033[91m'
 ENDC = '\033[0m'
 
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0'
+}
+
 def main():
-    thumb_file = 'thumbnail-gifs.txt'
-    for thumb in thumbnail_list(thumb_file):
+    r = requests.get('http://chilloutandwatchsomecatgifs.com/catgifs.txt')
+    catgifs = set()
+    for row in r.iter_lines():
+        link = row.strip()
+        if link and not link.startswith('#'):
+            catgifs.add(link)
+    for thumb in thumbnail_list('thumbnail-gifs.txt'):
+        cat = False
+        if thumb in catgifs:
+            cat = True
+            catgifs.remove(thumb)
         try:
-            r = requests.head(thumb)
+            r = requests.head(thumb, headers=headers)
         except Exception as e:
             print thumb, str(e)
+            if cat:
+              print FAIL + '  catgif.' + ENDC
             continue
         if r.status_code in [301, 302]:
             print thumb, FAIL + str(r.status_code)
             if r.headers['location'] != 'http://i.imgur.com/removed.png':
-                print '  ' + HEADER + r.headers['location'] + ENDC
+                print '  ' + HEADER + r.headers['location'] + ENDC,
             else:
                 print '  ' + HEADER + 'removed by imgur.' + ENDC
+            if cat:
+              print FAIL + '  catgif.' + ENDC
         elif r.status_code != 200:
             print thumb, FAIL + str(r.status_code) + ENDC
+            if cat:
+                print FAIL + '  catgif.' + ENDC
+    if len(catgifs):
+        print '\n' + FAIL + 'Missing Catgifs!' + ENDC
+        for thumb in catgifs:
+            print thumb
 
 
 def thumbnail_list(name = None):
