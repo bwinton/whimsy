a2fa7f46ab97ebc0f4c1fad38389bcc8b0ed75a0
Blake Winton <bwinton@latte.ca>
2015-05-15 16:41:55 -0400

    Add extra components.

---
+++ extra/d3promise.js
@@ -0,0 +1,51 @@
+/*! This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
+strict:true, undef:true, unused:true, curly:true, browser:true, white:true,
+moz:true, esnext:false, indent:2, maxerr:50, devel:true, node:true, boss:true,
+globalstrict:true, nomen:false, newcap:false */
+
+/*global d3:false, $:false */
+
+'use strict';
+
+(function () {
+  d3.csvPromise = function (url) {
+    var promise = $.Deferred();
+    d3.csv(url).get(function (error, rows) {
+      if (rows) {
+        promise.resolve(rows);
+      } else {
+        promise.reject(error);
+      }
+    });
+    return promise;
+  };
+
+  d3.jsonPromise = function (url) {
+    var promise = $.Deferred();
+    d3.json(url).get(function (error, data) {
+      if (data) {
+        promise.resolve(data);
+      } else {
+        promise.reject(error);
+      }
+    });
+    return promise;
+  };
+
+  d3.htmlPromise = function (url) {
+    var promise = $.Deferred();
+    d3.html(url).get(function (error, data) {
+      if (data) {
+        promise.resolve(data);
+      } else {
+        promise.reject(error);
+      }
+    });
+    return promise;
+  };
+})();
