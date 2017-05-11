e2fc755e1b97760f579b16d79af9c9253d32b3e6
Daniela Arcese <dnarcese@gmail.com>
2017-05-11 10:15:49 -0400

    fixed indenting and contributors list in manifest

--- background-scripts/mario.js
+++ background-scripts/mario.js
@@ -1,23 +1,23 @@
 'use strict';
 
 function onCreate(){
-   playSound("audio/coin.mp3");
+  playSound("audio/coin.mp3");
 }
 function onRemove(){
-   playSound("audio/goomba-stomp.wav");
+  playSound("audio/goomba-stomp.wav");
 }
 
 function playSound(url){
-   var getting = browser.storage.sync.get('mario');
-   getting.then((result)=>{
-      if (result.mario){
-         //play coin/goomba stomp sound
-         var audio = new Audio();
-         audio.src=browser.extension.getURL(url);
-         audio.autoplay=true;
-         audio.load();
-      }
-   });
+  var getting = browser.storage.sync.get('mario');
+  getting.then((result)=>{
+    if (result.mario){
+      //play coin/goomba stomp sound
+      var audio = new Audio();
+      audio.src=browser.extension.getURL(url);
+      audio.autoplay=true;
+      audio.load();
+    }
+  });
 }
 
 browser.bookmarks.onCreated.addListener(onCreate);
--- settings/options.js
+++ settings/options.js
@@ -2,20 +2,20 @@
 
 //save user's Mario mode preference in storage
 function saveOptions(){
-   browser.storage.sync.set({
-      mario: document.querySelector('#mario').checked
-   });
+  browser.storage.sync.set({
+    mario: document.querySelector('#mario').checked
+  });
 }
 //load Mario mode preference or set to default true
 function restoreOptions(){
-   var getting = browser.storage.sync.get('mario');
-   getting.then((result)=>{
-      document.querySelector("#mario").checked = result.mario || false;
-   });
+  var getting = browser.storage.sync.get('mario');
+  getting.then((result)=>{
+    document.querySelector("#mario").checked = result.mario || false;
+  });
 
-   function onError(error){
-      console.log('Error: ${error}');
-   }
+  function onError(error){
+    console.log('Error: ${error}');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
