acdb06a17e3dce7174148b897009f1222b51be3e
Blake Winton <bwinton@latte.ca>
2019-11-19 11:42:03 -0500

    Check catgifs, too.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,38 @@ extern crate reqwest;
 
 use failure::{Error, ResultExt};
 use rayon::prelude::*;
-use reqwest::{Client, header};
+use reqwest::{header, Client};
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::iter::FromIterator;
 
 pub type Result<T> = ::std::result::Result<T, Error>;
 const THUMBNAIL_FILE: &str = "thumbnail-gifs.txt";
+const USER_AGENT: &str =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0";
+
+fn get_catgifs() -> Result<HashSet<String>> {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::USER_AGENT,
+        header::HeaderValue::from_static(USER_AGENT),
+    );
+    let client = Client::builder().default_headers(headers).build()?;
+    let response = client
+        .get("https://chilloutandwatchsomecatgifs.github.io/catgifs.txt")
+        .send()?
+        .text()?;
+    Ok(response
+        .lines()
+        .filter(|url| !url.starts_with('#') && !url.trim().is_empty())
+        .map(|url| url.to_owned())
+        .collect())
+}
 
 fn get_urls(file: &str) -> Result<Vec<String>> {
-    let urls = File::open(file)
-        .context(format_err!("Error opening {}", file))?;
+    let urls = File::open(file).context(format_err!("Error opening {}", file))?;
     let mut buf_reader = BufReader::new(urls);
     let mut contents = String::new();
     buf_reader
@@ -28,10 +49,11 @@ fn get_urls(file: &str) -> Result<Vec<String>> {
 fn get_item(url: &str) -> Result<String> {
     // Get the html and build an Item.
     let mut headers = header::HeaderMap::new();
-        headers.insert(header::USER_AGENT, header::HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0"));
-    let client = Client::builder()
-        .default_headers(headers)
-        .build()?;
+    headers.insert(
+        header::USER_AGENT,
+        header::HeaderValue::from_static(USER_AGENT),
+    );
+    let client = Client::builder().default_headers(headers).build()?;
     let response = client.head(url).send()?;
 
     if response.status().is_success() {
@@ -46,9 +68,10 @@ fn get_item(url: &str) -> Result<String> {
 }
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
     let images = get_urls(THUMBNAIL_FILE)?;
+    // Also check for duplicate images here somehow?
     let items: Vec<_> = images
+        .clone()
         .par_iter()
         .map(|url| {
             if url.starts_with('#') || url.trim().is_empty() {
@@ -65,9 +88,32 @@ fn main() -> Result<()> {
         })
         .filter_map(|x| x)
         .collect();
+
+    println!("images: {}", images.len());
+    let mut all_urls: HashSet<String> = HashSet::from_iter(images);
+    println!("all_urls A: {}", all_urls.len());
+    all_urls.extend(items.clone());
+    println!("all_urls B: {}", all_urls.len());
+    let catgifs = get_catgifs()?;
+    println!("\ncatgifs A: {:?}", catgifs.len());
+    let missing: HashSet<String> = catgifs
+        .difference(&all_urls)
+        .map(|url| get_item(url).ok())
+        .filter_map(|x| x)
+        .collect();
+
+    let actually_missing: HashSet<&String> = missing
+        .difference(&all_urls).collect();
+
+    println!("catgifs B: {:?}", actually_missing.len());
     let mut output = File::create(THUMBNAIL_FILE)?;
     for item in items {
-        writeln!(output, "{}", item)?;
+            writeln!(output, "{}", item)?;
+    }
+    if !actually_missing.is_empty() {
+        for item in actually_missing {
+            writeln!(output, "{}", item)?;
+        }
     }
 
     Ok(())
